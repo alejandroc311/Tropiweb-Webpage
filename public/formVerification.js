6922f5c76889ae7acbae2e53b27282b463e1957e
alejandroc311 <alejandroc311@gmail.com>
2019-12-06 20:42:10 +0000

    added the carousel for the pricing page

---
+++ public/formVerification.js
@@ -0,0 +1,42 @@
+
+
+
+
+
+//FUNCTION THAT USES REGEX TO CHECK IF EMAIL IS PROVIDED AND FORMATTED CORRECTLY
+
+function validateEmail(providedEmail) {
+  var regexEmailCheck = /^([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
+  if (regexEmailCheck.test(providedEmail)) {
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+
+
+
+$('#emailFormInput').on('keydown keyup', function(e){
+  var providedEmail = $("#emailFormInput").val();
+
+  if ($.trim(providedEmail)==0) {
+    $('.btn').attr('disabled', true);
+    console.log('it checks if no input');
+  }
+  if (validateEmail(providedEmail)) {
+    $('.btn').removeAttr('disabled');
+  }
+  else {
+    console.log('it checks for alternate input');
+    $('.btn').attr('disabled',true);
+  }
+});
+
+/*$('#commentFormInput').on('keydown keyup', function(e){
+  var providedComment = $('#commentFormInput').val();
+  if ($.trim(providedEmail==0)) {
+    $('.btn').attr('disabled',true);
+  }
+
+});*/
---
+++ public/mobileCarousel.js
@@ -0,0 +1,36 @@
+$("#mobileCarousel").carousel({
+  interval: 10000
+})
+
+$("#mobileCarousel").on("touchstart", function(event){
+        var xClick = event.originalEvent.touches[0].pageX;
+    $(this).one("touchmove", function(event){
+        var xMove = event.originalEvent.touches[0].pageX;
+        if( Math.floor(xClick - xMove) > 3 ){
+            $(this).carousel('next');
+        }
+        else if( Math.floor(xClick - xMove) < -3 ){
+            $(this).carousel('prev');
+        }
+    });
+    $(".carousel").on("touchend", function(){
+            $(this).off("touchmove");
+    });
+});
+
+$(".carousel carousel-item").each(function(){
+  var minPerSlide = 3;
+  var nextElement = $(this).next();
+  if(!nextElement.length){
+    nextElement = $(this).siblings(":first");
+  }
+  nextElement.children(':first-child').clone().appendTo($(this));
+
+  for (var i = 0; i < minPerSlide; i++) {
+    nextElement = nextElement.next();
+    if(!nextElement.length){
+      nextElement = $(this).siblings(":first");
+    }
+    nextElement.children(":first-child").clone().appendTo($(this));
+  }
+});
