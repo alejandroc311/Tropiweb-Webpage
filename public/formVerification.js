b6abbaa84383db441ebe7a37e00c152b10646e04
alejandroc311 <alejandroc311@gmail.com>
2020-06-23 04:23:40 +0100

    debugging branch for f@cking double swipe issue on iOS

--- public/formVerification.js
+++ public/formVerification.js
@@ -1,10 +1,3 @@
-
-
-
-
-
-//FUNCTION THAT USES REGEX TO CHECK IF EMAIL IS PROVIDED AND FORMATTED CORRECTLY
-
 function validateEmail(providedEmail) {
   var regexEmailCheck = /^([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
   if (regexEmailCheck.test(providedEmail)) {
@@ -15,11 +8,19 @@ function validateEmail(providedEmail) {
   }
 }
 
+function validateComment(providedComment){
+  var comment = $("#commentFormInput").val();
+  if(comment.length <= 255 && comment.length >= 10 ){
+    return true;
+  }
+  else {
+    return false;
+  }
+}
 
 
 $('#emailFormInput').on('keydown keyup', function(e){
   var providedEmail = $("#emailFormInput").val();
-
   if ($.trim(providedEmail)==0) {
     $('.btn').attr('disabled', true);
     console.log('it checks if no input');
@@ -27,16 +28,35 @@ $('#emailFormInput').on('keydown keyup', function(e){
   if (validateEmail(providedEmail)) {
     $('.btn').removeAttr('disabled');
   }
-  else {
-    console.log('it checks for alternate input');
-    $('.btn').attr('disabled',true);
-  }
+
 });
 
-/*$('#commentFormInput').on('keydown keyup', function(e){
+$('#commentFormInput').on('keydown keyup', function(e){
   var providedComment = $('#commentFormInput').val();
-  if ($.trim(providedEmail==0)) {
+  if ($.trim(providedComment==0)) {
     $('.btn').attr('disabled',true);
+    console.log("it removed the attribute in email");
+  }
+  if (validateComment(providedComment)){
+    $('.btn').removeAttr('disabled');
+    console.log("it removed the attribute in comment");
   }
 
-});*/
+});
+
+$(function(){
+    $('.contactForm').submit(function(event){
+        event.preventDefault();
+        $.ajax({
+          url: '/',
+          type: 'post',
+          data: $('.contactForm').serialize(),
+          success: function(){
+            console.log('worked');
+            $('.btn').attr('disabled', true);
+            $('#emailFormInput').val('');
+            $('#commentFormInput').val('');
+          }
+        });
+    });
+  });
--- public/mobileCarousel.js
+++ public/mobileCarousel.js
@@ -4,27 +4,32 @@ $("#mobileCarousel").carousel({
 
 $("#mobileCarousel").on("touchstart", function(event){
         var xClick = event.originalEvent.touches[0].pageX;
+        console.log("touchstart is firing");
     $(this).one("touchmove", function(event){
         var xMove = event.originalEvent.touches[0].pageX;
         if( Math.floor(xClick - xMove) > 3 ){
             $(this).carousel('next');
+            console.log("next event is firing.");
         }
         else if( Math.floor(xClick - xMove) < -3 ){
             $(this).carousel('prev');
+            console.log("prev event is firing");
         }
     });
-    $(".carousel").on("touchend", function(){
+    $(this).one("touchend", function(){
             $(this).off("touchmove");
+            console.log("touchend is firing");
     });
 });
 
 $(".carousel carousel-item").each(function(){
-  var minPerSlide = 3;
+  var minPerSlide = 1;
   var nextElement = $(this).next();
   if(!nextElement.length){
     nextElement = $(this).siblings(":first");
   }
   nextElement.children(':first-child').clone().appendTo($(this));
+  console.log("first item event is firing");
 
   for (var i = 0; i < minPerSlide; i++) {
     nextElement = nextElement.next();
@@ -32,5 +37,6 @@ $(".carousel carousel-item").each(function(){
       nextElement = $(this).siblings(":first");
     }
     nextElement.children(":first-child").clone().appendTo($(this));
+    console.log("next item event is firing");
   }
 });
